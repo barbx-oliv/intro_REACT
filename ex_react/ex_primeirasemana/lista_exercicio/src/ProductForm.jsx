2f014013a18eca353c0f13a0605fa3b3bdec9711
barbx-oliv <barba.oliv8@gmail.com>
2026-09-17 16:59:57 -0300

    corrigindo e refazendo Productform

--- ex_react/ex_primeirasemana/lista_exercicio/src/ProductForm.jsx
+++ ex_react/ex_primeirasemana/lista_exercicio/src/ProductForm.jsx
@@ -2,43 +2,23 @@
 // 1. O formulário precisa cadastrar um novo produto com nome, preço e categoria 
 // 2. formulário precisa de nome, prçeo, categoria e imagem por props
 
+import { useState } from "react"
+
 const ProdutoForm = ({addProduto}) => {
 
-    const [textoProduto, setTextoProduto] = useState("");
+    // o const garante que o valor não mude o valor da variável de estado
+
+    const [nome, setNome] = useState("");
+    const [preco, setPreco] = useState("");
+    const [categoria, setCategoria] = useState("");
+    const [imagem, setImagem] = useState("");
 
     const handleSubmit = (e) => {
-        e.preventDefault();
-        if(!textoProduto.trim()) return; // se tiver vazio retorna
+        e.preventDefault(); // evita recarregamento 
+        if(!)
+    }
 
-        addProduto(textoProduto.trim());
-        setTextoProduto("") // limpa o campo de tarefas depois do envio 
-    };
-
-    return(
-        <form onSubmit={handleSubmit}>
-            <input 
-                type="text" 
-                placeholder="Digite nome do produto" 
-                value={textoProduto} 
-                onChange={(e) => setTextoProduto(e.target.value)} 
-            />
-
-            <input 
-                type="text"
-                placeholder="Digite o preço do produto"
-                value={textoProduto} 
-                onChange={(e) => setTextoProduto(e.target.value)}
-            />
-
-            <input 
-                type="text"
-                placeholder="Digite a categoria do produto"
-                value={textoProduto} 
-                onChange={(e) => setTextoProduto(e.target.value)}
-            />
-
-            <button type="submit">Adicionar</button>
-        </form>
-    );
 
 }
+
+export default ProdutoForm;
